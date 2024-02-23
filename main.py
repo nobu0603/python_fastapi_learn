a438f95c097fbe6daa4c2ec35a698267158d5e26
nobuki <nobking.63@gmail.com>
2024-02-23 12:00:00 +0900

    path paramater learn

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def index():
-    return {"message": "Hello World"}
+@app.get("/countries/japan")
+async def japan():
+    return {"country_name": 'This is Japan!'}
+
+@app.get("/countries/{country_name}")
+async def country(country_name: str):
+    return {"country_name": country_name}
