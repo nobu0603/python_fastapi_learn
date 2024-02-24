81400f88d5444bb4c8bb19a8456dedd95720a412
nobuki <nobking.63@gmail.com>
2024-02-24 17:06:14 +0900

    post method learn

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# app = FastAPI()
+
+# @app.get("/countries/")
+# async def country(country_name: Optional[str] = None, country_no: Optional[int] = None):
+#     return {
+#         "country_name": country_name,
+#         "country_no": country_no
+#     }
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: int
+    tax: Optional[float] = None
 
 app = FastAPI()
 
-@app.get("/countries/")
-async def country(country_name: Optional[str] = None, country_no: Optional[int] = None):
-    return {
-        "country_name": country_name,
-        "country_no": country_no
-    }
+@app.post("/item/")
+async def create_item(item: Item):
+    # return item
+    return {"message": f"{item.name}は、税込価格{int(item.price*item.tax)}円です。"}
