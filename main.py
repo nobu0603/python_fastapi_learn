d533511a9a06b361b618b01ed87a97d7c77b526c
nobuki <nobking.63@gmail.com>
2024-02-24 16:25:32 +0900

    query paramater learn

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/countries/japan")
-async def japan():
-    return {"country_name": 'This is Japan!'}
-
 @app.get("/countries/{country_name}")
-async def country(country_name: str):
-    return {"country_name": country_name}
+async def country(country_name: str = 'japan', city_name: str = 'tokyo'):
+    return {
+        "country_name": country_name,
+        "city_name": city_name
+    }
