ef3051494535f49dd448fa1701ee0a3e9b527d5f
nobuki <nobking.63@gmail.com>
2024-03-22 16:54:23 +0900

    Refactor main.py: Uncomment country endpoint and add missing imports

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from pydantic import BaseModel, Field
 
 # app = FastAPI()
 
-# @app.get("/countries/")
-# async def country(country_name: Optional[str] = None, country_no: Optional[int] = None):
-#     return {
-#         "country_name": country_name,
-#         "country_no": country_no
-#     }
+
+@app.get("/countries/")
+async def country(country_name: Optional[str] = None, country_no: Optional[int] = None):
+    return {"country_name": country_name, "country_no": country_no}
+
 
 class ShopInfo(BaseModel):
     name: str
     location: str
+
 
 class Item(BaseModel):
     name: str = Field(min_length=4, max_length=12)
@@ -21,9 +21,11 @@ class Item(BaseModel):
     price: int
     tax: Optional[float] = None
 
+
 class Data(BaseModel):
     shop_info: Optional[ShopInfo] = None
     items: List[Item]
+
 
 app = FastAPI()
 
@@ -31,6 +33,7 @@ app = FastAPI()
 # async def create_item(item: Item):
 #     # return item
 #     return {"message": f"{item.name}は、税込価格{int(item.price*item.tax)}円です。"}
+
 
 @app.post("/")
 async def index(data: Data):
